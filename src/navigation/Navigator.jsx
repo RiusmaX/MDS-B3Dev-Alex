24982315cd4ccb29910f2cf33e93c805716c8b80
Guillaume <guillaumecolloch56@gmail.com>
2023-05-05 13:51:27 +0200

    ajout de la page profil

--- src/navigation/Navigator.jsx
+++ src/navigation/Navigator.jsx
@@ -1,7 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native'
 import Home from '../screens/Home'
+import Profil from '../screens/Profil'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Icon from 'react-native-vector-icons/Ionicons'
+import Icon2 from 'react-native-vector-icons/FontAwesome5'
 import { Colors } from '../styles/Global'
 const TabStack = createBottomTabNavigator()
 
@@ -15,6 +17,14 @@ function Navigator () {
           options={{
             tabBarIcon: ({ focused, color, size }) =>
               <Icon name={focused ? 'home' : 'home-outline'} size={size} color={Colors.primary} />
+          }}
+        />
+        <TabStack.Screen
+          name='Profil'
+          component={Profil}
+          options={{
+            tabBarIcon: ({ focused, color, size }) =>
+              <Icon2 name={focused ? 'user' : 'user-circle'} size={size} color={Colors.primary} />
           }}
         />
       </TabStack.Navigator>
---
+++ src/screens/Profil.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import { styles } from '../styles/ProfilStyle'
+
+function Profil () {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Profil</Text>
+    </View>
+  )
+}
+
+export default Profil
