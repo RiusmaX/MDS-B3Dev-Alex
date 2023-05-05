ba4958b555f67ebc7406ba89725940d6dd78a840
KyllianHB <kyllianherbays@gmail.com>
2023-05-05 16:34:28 +0200

    feat: :art: Add db sqlite , fonction registerUserLocation , getUserLocation
    add function

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,16 @@
 import { View, Text } from 'react-native'
 import { styles } from '../styles/HomeStyle'
+import { useEffect } from 'react'
+import { getUserLocation, registerUserLocation } from '../database/db-service'
 
 function Home () {
+
+  useEffect(() => {
+    registerUserLocation(888,10,5)
+    getUserLocation().then(results => console.log(results))
+    // const location = getUserLocation()
+}, [])
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home</Text>
