3d9e81629dac4b020ab5a5e408f811af2941ddfe
KyllianHB <kyllianherbays@gmail.com>
2023-05-25 11:38:48 +0200

    Create table

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import { styles } from '../styles/HomeStyle'
 import { useEffect } from 'react'
-import { getUserLocation, registerUserLocation } from '../database/db-service'
+import { getUserLocation, registerUserLocation, createTables } from '../database/db-service'
 
 function Home () {
 
   useEffect(() => {
+    createTables()
     registerUserLocation(888,10,5)
     getUserLocation().then(results => console.log(results))
     // const location = getUserLocation()
