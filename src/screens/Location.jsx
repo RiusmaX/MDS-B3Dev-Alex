bb29f3b5277822b423f9f3697090b21cfaa1c8be
AlexandreDujardin <alexandre.dujardin14@orange.fr>
2023-05-25 10:32:22 +0200

    add location screen

--- src/screens/Location.jsx
+++ src/screens/Location.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { FlatList, PermissionsAndroid, StyleSheet, Text, View } from 'react-native'
-import BackgroundTask from 'react-native-background-task'
 import Geolocation from '@react-native-community/geolocation'
 
 function Location () {
@@ -20,27 +19,25 @@ function Location () {
         )
         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
           console.log('Location permission granted')
-          BackgroundTask.define(async () => {
-            const intervalId = setInterval(() => {
-              Geolocation.watchPosition(
-                (position) => {
-                  setLocations((prevLocations) => [...prevLocations, position])
-                  console.log(position)
-                },
-                (error) => {
-                  console.log(error)
-                },
-                {
-                  enableHighAccuracy: true,
-                  timeout: 15000,
-                  maximumAge: 10000,
-                  distanceFilter: 1,
-                  fastestInterval: 2000,
-                  showLocationDialog: true,
-                  useSignificantChanges: true
-                }
-              )
-            })
+          const intervalId = setInterval(() => {
+            Geolocation.watchPosition(
+              (position) => {
+                setLocations((prevLocations) => [...prevLocations, position])
+                console.log(position)
+              },
+              (error) => {
+                console.log(error)
+              },
+              {
+                enableHighAccuracy: true,
+                timeout: 15000,
+                maximumAge: 10000,
+                distanceFilter: 1,
+                fastestInterval: 2000,
+                showLocationDialog: true,
+                useSignificantChanges: true
+              }
+            )
             return () => clearInterval(intervalId)
           }, 1000)
         } else {
