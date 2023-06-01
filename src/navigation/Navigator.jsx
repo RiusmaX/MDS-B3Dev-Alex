5f3c529d0223521947103dcc9474ffe9ccccb26a
Guillaume <guillaumecolloch56@gmail.com>
2023-06-01 11:09:02 +0200

    ajout new screen with buttons

--- src/navigation/Navigator.jsx
+++ src/navigation/Navigator.jsx
@@ -1,11 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
 import Home from '../screens/Home'
-import Profil from '../screens/Informations'
+import Profil from '../screens/Profil'
 import EditProfil from '../screens/EditProfil' // Importez le composant EditProfil
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Icon from 'react-native-vector-icons/Ionicons'
 import Icon2 from 'react-native-vector-icons/FontAwesome5'
 import { Colors } from '../styles/Global'
+import Informations from '../screens/Informations'
 
 const TabStack = createBottomTabNavigator()
 
@@ -17,26 +18,34 @@ function Navigator () {
           name='Home'
           component={Home}
           options={{
-            tabBarIcon: ({ focused, color, size }) =>
+            tabBarIcon: ({ focused, color, size }) => (
               <Icon name={focused ? 'home' : 'home-outline'} size={size} color={Colors.primary} />
+            )
           }}
         />
         <TabStack.Screen
           name='Profil'
           component={Profil}
           options={{
-            tabBarIcon: ({ focused, color, size }) =>
+            tabBarIcon: ({ focused, size }) => (
               <Icon2 name={focused ? 'user' : 'user-circle'} size={size} color={Colors.primary} />
+            )
           }}
         />
         <TabStack.Screen
           name='EditProfil'
           component={EditProfil}
           options={{
-            tabBarVisible: false // Masquer la barre de navigation lors de l'édition du profil
+            tabBarVisible: false // Cacher le bouton dans la barre de navigation
           }}
         />
-
+        <TabStack.Screen
+          name='Informations'
+          component={Informations}
+          options={{
+            tabBarVisible: false // Cacher le bouton dans la barre de navigation
+          }}
+        />
       </TabStack.Navigator>
     </NavigationContainer>
   )
--- src/screens/Profil.jsx
+++ src/screens/Profil.jsx
@@ -1,52 +1,34 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image, Button } from 'react-native'
-import { profilData } from '../database/profilData'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 const Profil = ({ navigation }) => {
-  const onPressEdit = () => {
+  const onPressEditProfil = () => {
     navigation.navigate('EditProfil')
   }
 
-  const { name, email, age, country, occupation, profilePicUrl } = profilData[0]
+  const onPressInformations = () => {
+    navigation.navigate('Informations')
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{name}</Text>
-      <Image source={{ uri: profilePicUrl }} style={styles.image} />
-      <Text style={styles.text}>{`Nom : ${name}`}</Text>
-      <Text style={styles.text}>{`Email : ${email}`}</Text>
-      <Text style={styles.text}>{`Age : ${age}`}</Text>
-      <Text style={styles.text}>{`Pays : ${country}`}</Text>
-      <Text style={styles.text}>{`Travail : ${occupation}`}</Text>
-      <Button title='EditProfil' onPress={onPressEdit} />
+      <TouchableOpacity style={styles.button} onPress={onPressEditProfil}>
+        <Text style={styles.buttonText}>Modifier Profil</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={onPressInformations}>
+        <Text style={styles.buttonText}>Informations</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold'
-  },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    marginVertical: 20
-  },
-  text: {
-    height: 40,
-    width: '80%',
-    borderColor: 'gray',
-    borderWidth: 1,
+  button: {
+    backgroundColor: '#2196F3',
+    padding: 10,
     borderRadius: 5,
-    paddingHorizontal: 10,
-    marginVertical: 10
+    marginVertical: 10,
+    alignItems: 'center'
   }
 })
 
