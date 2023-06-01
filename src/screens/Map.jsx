489db55fcc8e0482bf17a3e508e78e6043cffd9d
Kim Robert <kimrobert.nb@gmail.com>
2023-06-01 16:49:22 +0200

    bkim-map wip true user location bug on compile with watch node permission

--- src/screens/Map.jsx
+++ src/screens/Map.jsx
@@ -1,6 +1,8 @@
 import { StyleSheet, View } from 'react-native'
 import MapboxGL from '@rnmapbox/maps'
-import AlertCard from '../components/AlertCard'
+import Geolocation from '@react-native-community/geolocation' // allows to use geolocation without expo
+import { useState } from 'react'
+
 const styles = StyleSheet.create({
   page: {
     flex: 1,
@@ -24,14 +26,22 @@ MapboxGL.setAccessToken(accessToken)
 // MapboxGL.setConnected(true)
 MapboxGL.requestAndroidLocationPermissions() // ask android permission to access user location
 MapboxGL.setTelemetryEnabled(false)
-
-const userLocationUpdate = () => {
-  console.log('Localisation mise à jour: ', MapboxGL.UserLocation.toString())
-}
-
-const coordinates = [47.2147, -1.5294]
+MapboxGL.locationManager.setLocationEventThrottle(2000)
 
 function Map () {
+  // user location
+  Geolocation.getCurrentPosition(info => console.log('localisation: ', info))
+  const [userLocation, setUserLocation] = useState([47.214677895892144, -1.5293022628039652])
+  const userLocationUpdate = () => {
+    // (PENDING: instead of using this location, use real location)
+    setUserLocation([45.214677895892144, -2.5293022628039652])
+  }
+  // alert location
+  const [alertLocation, setAlertLocation] = useState([47.21500292610005, -1.5260660347598858])
+  // const alertLocationUpdate = () => {
+  //   console.log('Localisation alerte mise à jour: ', MapboxGL.UserLocation.toString())
+  // }
+
   return (
     <View style={styles.page}>
       <View style={styles.container}>
@@ -41,20 +51,26 @@ function Map () {
           style={styles.map}
           logoEnabled={false} // hide logo
           attributionEnabled={false} // hide attribution
-          centerCoordinate={[47.2147, -1.5294]} // center map on user coordinates
           zoomEnabled
           pitchEnabled
           rotateEnabled
           scrollEnabled
           compassEnabled
         >
+          <MapboxGL.Camera
+            zoomLevel={15}
+            centerCoordinate={userLocation}
+            followUserLocation
+          />
 
           {/* User Geolocation dot */}
           <MapboxGL.UserLocation
             androidRenderMode='normal'
             animated
+            showsUserHeadingIndicator
             color='blue'
-            minDisplacement={10} // minimum displacement in meters to trigger geolocation update
+            coordinate={userLocation}
+            minDisplacement={5} // minimum displacement in meters to trigger geolocation update
             requestsAlwaysUse // requests to access to location at all times, even when app is in background
             onUpdate={userLocationUpdate} // callback function called when location is updated
           />
@@ -63,15 +79,10 @@ function Map () {
 
           {/* Locating alert with coordinates on the map */}
           {/* (PENDING: add a list of alerts and locate them all on the map) */}
-          <MapboxGL.MarkerView
+          {/* <MapboxGL.MarkerView
             coordinates={[47.2147, -1.5294]}
             color='red'
-          >
-
-            {/* interactible components must be passed as children of the location marker */}
-            <AlertCard />
-
-          </MapboxGL.MarkerView>
+          /> */}
         </MapboxGL.MapView>
       </View>
     </View>
